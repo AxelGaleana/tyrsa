401c197f75080ccd2f0f4a26f2ad4993c368556c
axelgaleanab <axelgaleanab@gmail.com>
2026-01-28 23:18:17 -0600

    Mejora en notificacion de correo

--- TYRSA_API/src/main/java/com/tyrsa/api_erp/service/EmailService.java
+++ TYRSA_API/src/main/java/com/tyrsa/api_erp/service/EmailService.java
@@ -16,40 +16,64 @@ public class EmailService {
 
     public void sendTemporaryPassword(String toEmail, String tempPassword) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("industrializacion@troqueladostyrsa.com");
         message.setTo(toEmail);
         message.setSubject("Contraseña temporal");
-        message.setText("Tu nueva contraseña temporal es: " + tempPassword + 
-                        "\nPor favor, cámbiala después de iniciar sesión.");
+        message.setText(
+            "Tu nueva contraseña temporal es: " + tempPassword +
+            "\n\nPor favor, cámbiala después de iniciar sesión." +
+            "\n\n----------------------------------" +
+            "\nSistema de Industrialización" +
+            "\nTyrsa Troquelados"
+        );
 
         mailSender.send(message);
     }
 
     public void sendPartChageAprobalRequest(String[] toEmails, String partNumber) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("industrializacion@troqueladostyrsa.com");
         message.setTo(toEmails);
         message.setSubject("Actualizacion de parte requiere su aprobacion");
-        message.setText("El item con número de parte: " + partNumber + " ha sido actualizado y requiere de su aprobación" +
-                        "\nPor favor, ingrese al sistema para ver los cambios.");
+        message.setText(
+            "El item con número de parte: " + partNumber + " ha sido actualizado y requiere de su aprobación" +
+            "\n\nPor favor, ingrese al sistema para ver los cambios." +
+            "\n\n----------------------------------" +
+            "\nSistema de Industrialización" +
+            "\nTyrsa Troquelados"
+        );
 
         mailSender.send(message);
     }
 
     public void newPartAdded(String[] toEmails, Part part) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("industrializacion@troqueladostyrsa.com");
         message.setTo(toEmails);
-        message.setSubject("Actualizacion de parte requiere su aprobacion");
-        message.setText("El item con numero de parte: " + part.getNumeroParte() + " y descripción '" + part.getDescripcion() + "'' ha sido dado de alta." +
-                        "\nIngresa al sistema para ver más detalles.");
+        message.setSubject("Alta de parte requiere su aprobacion");
+        message.setText(
+            "El item con numero de parte: " + part.getNumeroParte() + " y descripción '" + part.getDescripcion() + "'' ha sido dado de alta." +
+           "\n\nPor favor, ingrese al sistema para ver los cambios." +
+            "\n\n----------------------------------" +
+            "\nSistema de Industrialización" +
+            "\nTyrsa Troquelados"
+        );
 
         mailSender.send(message);
     }
 
     public void partUpdated(String[] toEmails, Part part) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("industrializacion@troqueladostyrsa.com");
         message.setTo(toEmails);
         message.setSubject("Actualizacion de parte requiere su aprobacion");
-        message.setText("El item con numero de parte: " + part.getNumeroParte() + " y descripción '" + part.getDescripcion() + "'' ha sido actualizado." +
-                        "\nIngresa al sistema para ver más detalles.");
+        message.setText(
+            "El item con numero de parte: " + part.getNumeroParte() + " y descripción '" + part.getDescripcion() + "'' ha sido actualizado." +
+           "\n\nPor favor, ingrese al sistema para ver los cambios." +
+            "\n\n----------------------------------" +
+            "\nSistema de Industrialización" +
+            "\nTyrsa Troquelados"
+        );
 
         mailSender.send(message);
     }
